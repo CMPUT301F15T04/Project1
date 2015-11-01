d98727701b3a7a439690c149f15eb8ab68b6f373
Blake Sakaluk <blake.sakaluk@gmail.com>
2015-11-01 15:20:15 -0700

    Added basic Cache class

---
+++ GameSwap/app/src/main/java/cmput301t4/gameswap/Cache.java
@@ -0,0 +1,25 @@
+package cmput301t4.gameswap;
+
+import java.lang.reflect.Array;
+
+/**
+ * Created by sakaluk on 11/1/15.
+ */
+public class Cache {
+    ArrayList<Item> itemsToBePushed;
+    ArrayList<Trade> tradesToBePushed;
+    int accountID;
+    Trader friend;
+
+    public void addItemsToCache(ArrayList<Item> items) {
+
+    }
+
+    public void addTradesToCache(ArrayList<Trade> trades) {
+
+    }
+
+    public void pullFriend(Trader friend) {
+
+    }
+}
