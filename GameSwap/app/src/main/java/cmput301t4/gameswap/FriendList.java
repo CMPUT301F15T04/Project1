919f47de255e558ab00769758f28fe83950217a6
Blake Sakaluk <blake.sakaluk@gmail.com>
2015-11-01 15:44:21 -0700

    Started implementation of FriendList

--- GameSwap/app/src/main/java/cmput301t4/gameswap/FriendList.java
+++ GameSwap/app/src/main/java/cmput301t4/gameswap/FriendList.java
@@ -6,13 +6,19 @@ import java.util.ArrayList;
  * Created by sakaluk on 11/1/15.
  */
 public class FriendList {
-    ArrayList<Trader> FriendCollection;
+    ArrayList<Trader> friendCollection;
 
     public void AddFriend(int FriendID) {
-
+        // TODO: Pull Trader from server by ID
+        Trader friend = new Trader();
+        friendCollection.add(friend);
     }
 
     public void DeleteFriend(int FriendID) {
-
+        for(int i = 0; i < friendCollection.size(); i++) {
+            if(friendCollection.get(i).getID() == friendID) {
+                friendCollection.remove(friend);
+            }
+        }
     }
 }
