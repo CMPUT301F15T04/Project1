b86ef9a87f7754f299f5891f69c4ea7c33143f0c
Blake Sakaluk <blake.sakaluk@gmail.com>
2015-11-01 15:09:13 -0700

    Made basic TradeList class

---
+++ GameSwap/app/src/main/java/cmput301t4/gameswap/FriendList.java
@@ -0,0 +1,18 @@
+package cmput301t4.gameswap;
+
+import java.util.ArrayList;
+
+/**
+ * Created by sakaluk on 11/1/15.
+ */
+public class FriendList {
+    ArrayList<Trader> FriendCollection;
+
+    public void AddFriend(int FriendID) {
+
+    }
+
+    public void DeleteFriend(int FriendID) {
+
+    }
+}
