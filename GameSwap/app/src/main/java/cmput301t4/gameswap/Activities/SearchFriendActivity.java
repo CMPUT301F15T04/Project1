d6adb1f7853060207c7cf119418fa3afc39f5c3a
Rupehra Chouhan <rupehra1@gmail.com>
2015-11-04 17:17:05 -0700

    UI

--- GameSwap/app/src/main/java/cmput301t4/gameswap/Activities/SearchFriendActivity.java
+++ GameSwap/app/src/main/java/cmput301t4/gameswap/Activities/SearchFriendActivity.java
@@ -2,17 +2,39 @@ package cmput301t4.gameswap.Activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 import cmput301t4.gameswap.R;
 
 public class SearchFriendActivity extends Activity {
 
+    private ArrayAdapter<String> adapter;
+    private ListView stringList;
+    private ArrayList<String> names;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_friend);
+
+        stringList = (ListView) findViewById(R.id.listView);
+        names = new ArrayList<String>();
+
+        String[] data = {"Rupehra","Kynan","Blake","Preyanshu","Daniel","a","b"};
+       // names.addAll(data);
+        adapter = new ArrayAdapter<String>(this,R.layout.listviewtext,names);
+
+        stringList.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+
+
+
     }
 
     @Override
