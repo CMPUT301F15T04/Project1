72f569ed2e009f7fa80e35072bcfb45328896802
Blake Sakaluk <blake.sakaluk@gmail.com>
2015-11-05 15:25:50 -0700

    Set up CreateProfileActivity

---
+++ GameSwap/app/src/main/java/cmput301t4/gameswap/Activities/CreateProfileActivity.java
@@ -0,0 +1,34 @@
+package cmput301t4.gameswap.Activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import cmput301t4.gameswap.Models.User;
+import cmput301t4.gameswap.R;
+
+/**
+ * Created by Blake on 2015-11-05.
+ */
+public class CreateProfileActivity extends Activity {
+    private User myUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_profile);
+    }
+
+    public void createProfileButton(View view) {
+        EditText usernameEditText = (EditText) findViewById(R.id.username);
+        EditText emailEditText = (EditText) findViewById(R.id.email_user);
+        EditText cityEditText = (EditText) findViewById(R.id.user_city);
+        EditText phoneNumberEditText = (EditText) findViewById(R.id.user_phone_num);
+        myUser = new User(usernameEditText.getText().toString(), emailEditText.getText().toString(), cityEditText.getText().toString(), phoneNumberEditText.getText().toString());
+    }
+
+    public User getUser() {
+        return myUser;
+    }
+}
