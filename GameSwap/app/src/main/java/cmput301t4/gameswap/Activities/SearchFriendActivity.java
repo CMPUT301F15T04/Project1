a2425e6972d6af1d5feaaccd8c951aec9075154f
Rupehra Chouhan <rupehra1@gmail.com>
2015-11-06 13:51:19 -0700

    UI

--- GameSwap/app/src/main/java/cmput301t4/gameswap/Activities/SearchFriendActivity.java
+++ GameSwap/app/src/main/java/cmput301t4/gameswap/Activities/SearchFriendActivity.java
@@ -64,7 +64,6 @@ public class SearchFriendActivity extends Activity {
                                 Toast.makeText(getBaseContext(), item.getTitle(), Toast.LENGTH_SHORT).show();
                                 return true;
 
-
                         }
 
 
