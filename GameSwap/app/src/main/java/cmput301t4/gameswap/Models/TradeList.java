1d1a4aad74e4c010bcebbb718e51c4f79a048ef8
preyansh <preyansh@ualberta.ca>
2015-11-12 23:17:17 -0700

    Finished up most of TradeList for javadocs

--- GameSwap/app/src/main/java/cmput301t4/gameswap/Models/TradeList.java
+++ GameSwap/app/src/main/java/cmput301t4/gameswap/Models/TradeList.java
@@ -6,36 +6,59 @@ import cmput301t4.gameswap.Models.Trade;
 
 /**
  * Created by kynan on 11/1/15.
+ * Model for list of trades that a User will have
  */
 public class TradeList {
+    /** The trade list of all current and past trades of the user */
     private ArrayList<Trade> tradelist = new ArrayList<Trade>();
 
     //======Basic Function=====//
+    /**
+    * Adds a trade to trade list
+    * @param trade The trade that was done
+    */
     public void add(Trade trade) {
         tradelist.add(trade);
     }//end add to tradelist
 
+    /**
+     * Deletes a trade from the trade list
+     * @param position The position of hte trade the user wants to delete
+     */
     //This del the trade if you have trade object already, most likely will
     //be changed later to take in position
     public void del(int position) {
         tradelist.remove(tradelist.get(position));
     }//end del
 
+    /**
+     * Gets the trade the user wished to view from trade list
+     * @param index position of the wanted trade
+     */
     //Retrieve Trade at index
     public Trade getTrade(int index){
         return tradelist.get(index);
     }//End getTrade
 
+    /**
+     * Checks if there is a trade in trade list
+     * @param trade The trade that was done
+     */
     //See if tradelist contain trade
     public boolean hasTrade(Trade trade) {
         return tradelist.contains(trade);
     }//end hasTrade
 
+    /**
+     * Clears the tradelist
+     */
     //Wrote this in for testing, Not sure if we really need
     public void clearTradelist(){
         tradelist.clear();
     }//end clearTradelist
-
+    /**
+     * Checks if trade list is empty
+     */
     public boolean isEmpty(){return tradelist.isEmpty();}//end isEmpty
 
     public int getIndex(Trade trade){
