cfcffbb33890e3ce309ba1200b1a2f6c69eb4a69
Rupehra Chouhan <rupehra1@gmail.com>
2015-11-19 20:56:09 -0700

    editing profile

--- GameSwap/app/src/main/java/cmput301t4/gameswap/Activities/CreateProfileActivity.java
+++ GameSwap/app/src/main/java/cmput301t4/gameswap/Activities/CreateProfileActivity.java
@@ -2,6 +2,7 @@ package cmput301t4.gameswap.Activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
@@ -15,6 +16,11 @@ import cmput301t4.gameswap.R;
  */
 public class CreateProfileActivity extends Activity {
     private User myUser;
+    private EditText usernameEditText;
+    private EditText emailEditText;
+    private EditText cityEditText;
+    private EditText phoneNumberEditText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +29,10 @@ public class CreateProfileActivity extends Activity {
     }
 
     public void createProfileButton(View view) {
-        EditText usernameEditText = (EditText) findViewById(R.id.username);
-        EditText emailEditText = (EditText) findViewById(R.id.email_user);
-        EditText cityEditText = (EditText) findViewById(R.id.user_city);
-        EditText phoneNumberEditText = (EditText) findViewById(R.id.user_phone_num);
+        usernameEditText = (EditText) findViewById(R.id.username);
+        emailEditText = (EditText) findViewById(R.id.email_user);
+        cityEditText = (EditText) findViewById(R.id.user_city);
+        phoneNumberEditText = (EditText) findViewById(R.id.user_phone_num);
         UserManager.createUser(usernameEditText.getText().toString(), emailEditText.getText().toString(), cityEditText.getText().toString(), phoneNumberEditText.getText().toString(), this);
         ServerManager.saveUserOnline(UserManager.getTrader());
         this.finish();
@@ -37,6 +43,8 @@ public class CreateProfileActivity extends Activity {
     }
 
     public User getUser() {
+
+        Log.w("myUser",myUser.getUserName());
         return myUser;
     }
 }
--- GameSwap/app/src/main/java/cmput301t4/gameswap/Activities/TopTradesActivity.java
+++ GameSwap/app/src/main/java/cmput301t4/gameswap/Activities/TopTradesActivity.java
@@ -1,27 +1,24 @@
 package cmput301t4.gameswap.Activities;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import cmput301t4.gameswap.R;
 
-public class selectTaskActivity extends Activity {
+public class TopTradesActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_select_task);
+        setContentView(R.layout.activity_top_trades);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        //super.onCreateOptionsMenu(menu);
-        getMenuInflater().inflate(R.menu.menu_select_task, menu);
+        getMenuInflater().inflate(R.menu.menu_top_trades, menu);
         return true;
     }
 
@@ -36,27 +33,7 @@ public class selectTaskActivity extends Activity {
         if (id == R.id.action_settings) {
             return true;
         }
-        else if(id == R.id.my_profile){
-            Intent intent = new Intent(selectTaskActivity.this, MyProfileActivity.class);
-            startActivity(intent);
-        }
 
         return super.onOptionsItemSelected(item);
-    }
-
-
-
-
-    public void openInventoryButton(View view){
-        Intent intent = new Intent(selectTaskActivity.this,myInventoryActivity.class);
-        startActivity(intent);
-    }
-    public void openFriendsButton(View view){
-        Intent intent = new Intent(selectTaskActivity.this,FriendsNTradingActivity.class);
-        startActivity(intent);
-    }
-    public void tradeOffersButton(View view){
-        Intent intent = new Intent(selectTaskActivity.this,TradesActivity.class);
-        startActivity(intent);
     }
 }
