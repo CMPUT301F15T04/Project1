75cd7dd15b9119067d6aedb8add9e4e55bdd59fe
Rupehra Chouhan <rupehra1@gmail.com>
2015-11-30 16:07:25 -0700

    accepting trade

--- GameSwap/app/src/main/java/cmput301t4/gameswap/Activities/CounterTradeActivity.java
+++ GameSwap/app/src/main/java/cmput301t4/gameswap/Activities/CounterTradeActivity.java
@@ -32,7 +32,7 @@ public class CounterTradeActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_decide_trade);
+        setContentView(R.layout.activity_counter_trade);
         Intent intent = getIntent();
         Bundle b = intent.getExtras();
 
